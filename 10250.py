224156ed8df15eeff958330d29c9ed1e1ae350d7
L-and <ksw1812@naver.com>
2021-07-04 03:07:08 +0900

    solve
    1193, 2869, 10250

--- 10250.py
+++ 10250.py
@@ -1,18 +1,30 @@
 case = int(input())
 
 for i in range(case):
-    floors, rooms, N = input().split()
-    floors = int(floors)
-    rooms = int(rooms)
-    N = int(N)
+    height, width, n = input().split()
+    height = int(height)
+    width = int(width)
+    n = int(n)
 
-    if floors == 1:
-        answer = 100 + N
-    elif rooms == 1:
-        answer = 100 * N + 1
-    elif floors == N:
-        answer = floors * 100 + 1
-    else:
-        answer = int((N % floors) * 100 + (N / floors) + 1)
+    room = [[0 for i in range(width)] for j in range(height)]
 
-    print(answer)
+    for i in range(height):
+        for j in range(width):
+            room[i][j] = (i + 1) * 100 + (j + 1)
+
+    current_n = 0
+    current_height = 0
+    current_width = 0
+
+    while current_n < n - 1:
+        if current_height < height - 1:
+            current_n = current_n + 1
+            current_height = current_height + 1
+        elif current_height == height - 1:
+            current_n = current_n + 1
+            current_height = 0
+            current_width = current_width + 1
+        # print(current_height, current_width, current_n)
+        # input()
+    
+    print(room[current_height][current_width])
